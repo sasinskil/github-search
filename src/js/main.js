7e542c3a0e5690f3d2919c754303b1cba2be44ab
sasinskil <sasinskil@gmail.com>
2019-08-13 23:35:21 +0200

    using github api - fetch data

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,42 @@ const resultsContainer = document.querySelector('.results--js');
 const resultsList = document.querySelector('.results__list--js');
 const searchAgainButton = document.querySelector('.results__button--js');
 
+
+const findRepositories = () => {
+  const searchInputValue = searchInput.value;
+
+  fetch(`https://api.github.com/users/${searchInputValue}/repos`)
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+  
+    if(repos.length > 0){
+      mainHeader.classList.add('heading--hide');
+      resultsContainer.classList.add('results--visible');
+    }
+    
+    for(let repo of repos) {
+      const {name, homepage} = repo;
+      
+      if(homepage) {
+        resultsList.innerHTML += `<li class="item"><strong>Repo name</strong> ${name} <strong>Live</strong> <a class="results__link" href="${homepage}">Click!</a></li>`
+    } else {
+      resultsList.innerHTML += `<li class="results__item"><strong class="results__item--strong">Repo name</strong> ${name}</li>`
+    }
+    }
+})
+  .catch(err => alert(`Nie znaleziono użytkownika ${searchInputValue}, ${err}`));
+  searchInput.value="";
+}
+
+searchInput.addEventListener('keyup',(e) => {
+  if(e.keyCode == '13') {
+    findRepositories();
+  }
+})
+
+searchAgainButton.addEventListener('click', ()=> {
+  mainHeader.classList.remove('heading--hide');
+  resultsContainer.classList.remove('results--visible');
+  resultsList.innerHTML="";
+});
